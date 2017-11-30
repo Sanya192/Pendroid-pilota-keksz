b79ec4e124339229d335047c0d2b6fd3e18d0669
Sanyi <sanyika192@live.com>
2017-11-30 10:55:08 +0100

    gravitáció változtatása és egy egyszerű célpont hozzáadása

---
+++ Assets/Resources/Scripts/Target.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent (typeof(BoxCollider2D))]
+public class Target : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.name.IndexOf("Ball")>=0)
+            Destroy(gameObject);
+
+    }
+}
---
+++ Assets/Resources/Scripts/UI.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI : MonoBehaviour {
+
+    static Slider gravity;
+	// Use this for initialization
+	void Start () {
+        gravity = FindObjectOfType<Slider>();
+        gravity.onValueChanged.AddListener(delegate { GravityChange(); });
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void GravityChange(){
+        Physics.gravity = new Vector3(0,-9.8F*gravity.value,0);
+        Debug.Log(Physics.gravity);
+    }
+}
