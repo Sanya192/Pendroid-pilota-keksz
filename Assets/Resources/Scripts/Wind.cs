8c2982621db3c85a970be857faa9d048d579c084
PeterK98 <PeterK98@users.noreply.github.com>
2017-12-04 21:19:14 +0100

    Wind update

--- Assets/Resources/Scripts/Wind.cs
+++ Assets/Resources/Scripts/Wind.cs
@@ -7,7 +7,7 @@ public class Wind : MonoBehaviour {
 
     [Tooltip("WindSpeed between 0 and 250 (0 means no wind)")]
     [Range(0f,250f)]
-    public float WindSpeed = 50f;
+    public float WindSpeed = 0f;
 
     void OnTriggerStay2D(Collider2D other) {
         other.GetComponent<Rigidbody2D>().AddForce(-Vector2.right * WindSpeed * Time.deltaTime);
