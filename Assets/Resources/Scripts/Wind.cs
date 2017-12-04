38fe4679c4409b6f2c6612d594ed41cdf60bf3d8
PeterK98 <PeterK98@users.noreply.github.com>
2017-12-04 20:40:45 +0100

    Wind Added

---
+++ Assets/Resources/Scripts/Wind.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class Wind : MonoBehaviour {
+
+    public float WindSpeed = 50f;
+
+    void OnTriggerStay2D(Collider2D other) {
+        other.GetComponent<Rigidbody2D>().AddForce(-Vector2.right * WindSpeed * Time.deltaTime);
+    }
+}
