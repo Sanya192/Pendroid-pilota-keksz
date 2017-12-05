07c575ff804648385168cf57843960171617ccd7
PeterK98 <PeterK98@users.noreply.github.com>
2017-12-05 16:16:38 +0100

    deleted .vs folder

--- Assets/Resources/Scripts/UI.cs
+++ Assets/Resources/Scripts/UI.cs
@@ -10,8 +10,8 @@ public class UI : MonoBehaviour {
 
     static Slider gravitySlider;
     static Slider windSlider;
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start() {
         var sliders = FindObjectsOfType<Slider>();
         gravitySlider = sliders.First(P => P.name == "GravitySlider");
         windSlider = sliders.First(P => P.name == "WindSlider");
@@ -22,11 +22,10 @@ public class UI : MonoBehaviour {
 
     void GravityChange() {
         var ball = GameObject.FindGameObjectsWithTag("Ball");
-        Physics2D.gravity = new Vector2(0,-9.8F*gravitySlider.value);
+        Physics2D.gravity = new Vector2(0, -9.8F * gravitySlider.value);
         Debug.Log(Physics2D.gravity);
         for (int i = 0; i < ball.Length; i++) {
-             ball[i].GetComponent<Rigidbody2D>().Sleep();
-            
+            ball[i].GetComponent<Rigidbody2D>().Sleep();
         }
     }
 
