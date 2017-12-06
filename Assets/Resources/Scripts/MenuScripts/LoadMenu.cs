0647fd2ab3fd9019f9188d5209a04c9572260f00
PeterK98 <PeterK98@users.noreply.github.com>
2017-12-06 17:38:02 +0100

    splash

---
+++ Assets/Resources/Scripts/MenuScripts/LoadMenu.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadMenu : MonoBehaviour {
+
+    // Use this for initialization
+    void Start() {
+        Invoke("LoadMenuScene", 1f);
+    }
+
+    void LoadMenuScene() {
+        Application.LoadLevel("Game");
+    }
+}
