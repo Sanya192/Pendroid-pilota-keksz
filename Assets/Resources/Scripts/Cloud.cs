74868e3750dfe85532c40bc4e59e26fc33fc5e60
PeterK98 <PeterK98@users.noreply.github.com>
2017-12-07 17:10:01 +0100

    Felhők
    Textúra, spawner

---
+++ Assets/Resources/Scripts/Cloud.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cloud : MonoBehaviour {
+
+    public float startSpeed = 0.2f;
+    void Start() {
+        GetComponent<Rigidbody2D>().velocity = Vector2.left * startSpeed;
+    }
+
+    /// <summary>
+    /// OnBecameInvisible is called when the renderer is no longer visible by any camera.
+    /// </summary>
+    void OnBecameInvisible() {
+        Destroy(gameObject);
+    }
+
+}
---
+++ Assets/Resources/Scripts/CloudSpawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudSpawner : MonoBehaviour {
+
+    public GameObject cloudPrefab;
+    public float waitBeforaStart = 2f;
+    public float repeatTime = 5f;
+
+    public float z = 0;
+    // Use this for initialization
+    void Start() {
+        InvokeRepeating("Spawn", waitBeforaStart, repeatTime);
+    }
+
+    private void Spawn() {
+        float x = gameObject.transform.position.x + Random.Range(-1, 1);
+        float y = gameObject.transform.position.y + Random.Range(-1, 1);
+        Instantiate(cloudPrefab, new Vector3(x, y, z), Quaternion.identity);
+    }
+}
