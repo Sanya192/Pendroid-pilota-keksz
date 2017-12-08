ee7c2d51adddd88f4dd256c8c800dce7426fb3bb
PeterK98 <PeterK98@users.noreply.github.com>
2017-12-08 20:46:43 +0100

    windspeed

--- Assets/Resources/Scripts/Wind.cs
+++ Assets/Resources/Scripts/Wind.cs
@@ -6,8 +6,8 @@ using UnityEngine;
 public class Wind : MonoBehaviour {
 
     [Tooltip("WindSpeed between 0 and 250 (0 means no wind)")]
-    [Range(0f, 250f)]
-    public float WindSpeed = 0f;
+    [Range(10f, 100f)]
+    public float WindSpeed = 10f;
 
     void OnTriggerStay2D(Collider2D other) {
         other.GetComponent<Rigidbody2D>().AddForce(-Vector2.right * WindSpeed * Time.deltaTime);
